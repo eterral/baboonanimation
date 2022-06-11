2bbb10b2e7d6ff6e87cf48f418c5c49f00407e59
eterral <elliott@eterral.com>
2022-06-11 10:36:39 -0500

    fixed overlay for work

--- front-end/client/src/components/WorkCard/WorkCard.js
+++ front-end/client/src/components/WorkCard/WorkCard.js
@@ -4,22 +4,14 @@ import { useState } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 export default function WorkCard(props) {
-  const [isShown, setIsShown] = useState(false);
-
   return (
-    <TransitionGroup
-      className="work-container"
-      onMouseEnter={() => setIsShown(true)}
-      onMouseLeave={() => setIsShown(false)}
-    >
+    <TransitionGroup className="work-container">
       <CSSTransition timeout={500} classNames="work-box" key={props.id}>
         <div className="work-box">
           <img className="work-image" src={props.image} />
-          {isShown && (
-            <div className="work-overlay">
-              <p className="overlay-text">{props.text}</p>
-            </div>
-          )}
+          <div className="work-overlay">
+            <p className="overlay-text">{props.text}</p>
+          </div>
         </div>
       </CSSTransition>
     </TransitionGroup>
--- front-end/client/src/screens/Home/Home.js
+++ front-end/client/src/screens/Home/Home.js
@@ -37,7 +37,7 @@ export default function Home() {
           credits on dozens of the most popular animated films and series for
           families and teens. <b>Learn More</b>
         </p>
-        <Carousel
+        {/* <Carousel
           additionalTransfrom={0}
           arrows={false}
           autoPlay
@@ -86,6 +86,53 @@ export default function Home() {
           slidesToSlide={0.5}
           swipeable
           transitionDuration={1000}
+        > */}
+        <Carousel
+          additionalTransfrom={0}
+          arrows
+          autoPlaySpeed={1000}
+          centerMode={true}
+          className=""
+          containerClass="container"
+          dotListClass=""
+          draggable
+          focusOnSelect={false}
+          infinite
+          itemClass=""
+          keyBoardControl
+          minimumTouchDrag={80}
+          renderButtonGroupOutside={false}
+          renderDotsOutside={false}
+          responsive={{
+            desktop: {
+              breakpoint: {
+                max: 3000,
+                min: 1024,
+              },
+              items: 1,
+              partialVisibilityGutter: 20,
+            },
+            mobile: {
+              breakpoint: {
+                max: 464,
+                min: 0,
+              },
+              items: 1,
+              partialVisibilityGutter: 30,
+            },
+            tablet: {
+              breakpoint: {
+                max: 1024,
+                min: 464,
+              },
+              items: 2,
+              partialVisibilityGutter: 30,
+            },
+          }}
+          showDots={false}
+          sliderClass=""
+          slidesToSlide={1}
+          swipeable
         >
           <div className="carousel-img">
             <img className="cimg" src={powerpuff_headliner} />
