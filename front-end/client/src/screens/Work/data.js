c8e44cf378f85dd1840804b51fc3f9c8d5089f76
eterral <elliott@eterral.com>
2022-09-20 13:57:01 -0500

    pushing up work images

--- front-end/client/src/screens/Work/data.js
+++ front-end/client/src/screens/Work/data.js
@@ -8,16 +8,11 @@ export default [
   },
   {
     image:
-      "/work/WEBSITE ART A Stork's Journey 2 CK 060822 VOICE FEATURE DEVELOPMENT.png",
-    branded: "true",
-    voice: "true",
-    originals: "true",
-    series: "true",
-    features: "false",
-    featuressDesc:
+      "front-end/client/public/work/WEBSITE ART A Stork's Journey NB LS 052622 VOICE WRITING FEATURE DEVELOPMENT.png",
+    featuresDesc:
       "Baboon script-doctored and directed voices on this hit European film starring Jane Lynch and Drake Bell.",
     key: 2,
-    category: "features, voice, series",
+    category: "features, voice, series, development",
   },
   {
     image:
@@ -27,56 +22,31 @@ export default [
     originals: "true",
     series: "true",
     features: "false",
-    seriesDesc: "testing this",
+    featuresDesc:
+      "Baboon script-doctored, cast, and voiced this European sequel to the hit Jane Lynch Film.",
     key: 3,
-    category: "branded, series",
+    category: "voice, features, development",
   },
   {
-    image:
-      "/work/WEBSITE ART A Stork's Journey 2 CK 060822 VOICE FEATURE DEVELOPMENT.png",
-    branded: "true",
-    voice: "true",
-    originals: "true",
-    series: "true",
-    features: "false",
-    seriesDesc: "testing this",
-    key: 4,
-    category: "originals, voice, series",
-  },
-  {
-    image:
-      "/work/WEBSITE ART A Stork's Journey 2 CK 060822 VOICE FEATURE DEVELOPMENT.png",
-    branded: "true",
-    voice: "true",
-    originals: "true",
-    series: "true",
-    features: "true",
-    seriesDesc: "testing this",
+    image: "/work/WEBSITE ART Aerover KT 060622 VOICE DIVERSITY.png",
+    featuresDesc:
+      "Baboon adapted and voiced this series from Lion Forge Animation.",
     key: 5,
-    category: "features, voice, series",
+    category: "diversity, voice",
   },
   {
     image:
-      "/work/WEBSITE ART A Stork's Journey 2 CK 060822 VOICE FEATURE DEVELOPMENT.png",
-    branded: "true",
-    voice: "true",
-    originals: "true",
-    series: "true",
-    features: "false",
-    seriesDesc: "testing this",
+      "/work/WEBSITE ART Agent 203 KT 052622 LS 060722 VOICE WRITING SERIES DEVELOPMENT.png",
+    featuresDesc:
+      "Baboon developed, wrote, cast, and voice-directed this Super RTL series.",
     key: 6,
-    category: "features, voice, series, branded",
+    category: "development, voice, series, writing",
   },
   {
     image:
-      "/work/WEBSITE ART A Stork's Journey 2 CK 060822 VOICE FEATURE DEVELOPMENT.png",
-    branded: "true",
-    voice: "true",
-    originals: "true",
-    series: "true",
-    features: "false",
-    seriesDesc: "testing this",
+      "/work/WEBSITE ART Amazing world of Gumball FC 051822 VOICE WRITING SERIES.png",
+    seriesDesc: "Russell Marcus, Voice Actor \n Tobi Wilson, Writer",
     key: 7,
-    category: "voice, series",
+    category: "voice, writing, series",
   },
 ];
