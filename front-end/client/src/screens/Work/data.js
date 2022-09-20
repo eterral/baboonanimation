77c3ff3f8b6390a8b27a4dc9ebc60047c0f9a8b4
eterral <elliott@eterral.com>
2022-09-20 14:13:49 -0500

    pushing up work images

--- front-end/client/src/screens/Work/data.js
+++ front-end/client/src/screens/Work/data.js
@@ -8,7 +8,7 @@ export default [
   },
   {
     image:
-      "front-end/client/public/work/WEBSITE ART A Stork's Journey NB LS 052622 VOICE WRITING FEATURE DEVELOPMENT.png",
+      "/work/WEBSITE ART A Stork's Journey NB LS 052622 VOICE WRITING FEATURE DEVELOPMENT.png",
     featuresDesc:
       "Baboon script-doctored and directed voices on this hit European film starring Jane Lynch and Drake Bell.",
     key: 2,
@@ -17,11 +17,6 @@ export default [
   {
     image:
       "/work/WEBSITE ART A Stork's Journey 2 CK 060822 VOICE FEATURE DEVELOPMENT.png",
-    branded: "true",
-    voice: "true",
-    originals: "true",
-    series: "true",
-    features: "false",
     featuresDesc:
       "Baboon script-doctored, cast, and voiced this European sequel to the hit Jane Lynch Film.",
     key: 3,
@@ -45,8 +40,137 @@ export default [
   {
     image:
       "/work/WEBSITE ART Amazing world of Gumball FC 051822 VOICE WRITING SERIES.png",
-    seriesDesc: "Russell Marcus, Voice Actor \n Tobi Wilson, Writer",
+    featuresDesc: "Russell Marcus, Voice Actor \n Tobi Wilson, Writer",
     key: 7,
     category: "voice, writing, series",
+  },
+  {
+    image:
+      "/work/WEBSITE ART Angry Birds Toons JD LS 060722 WRITING SERIES BRANDS DEVELOPMENT.png",
+    featuresDesc: "Mike de Seve, Script Editor, Writer\nJoe Vitale, Writer",
+    key: 8,
+    category: "writing, series, branded, development",
+  },
+  {
+    image:
+      "/work/WEBSITE ART Animal Jam JD 052422 KT 060922 062222 BRANDS DEVELOPMENT.png",
+    featuresDesc: "Baboon developed this series for Nat Geo Kids.",
+    key: 9,
+    category: "branded, development",
+  },
+  {
+    image:
+      "/work/WEBSITE ART Bayala NB LS 060722 VOICE WRITING FEATURE BRANDS DEVELOPMENT.png",
+    featuresDesc:
+      "Baboon script doctored and wrote on this family film for Fabrique D'Images and Ulysses Films.",
+    key: 10,
+    category: "voice, writing, features, branded, development",
+  },
+  {
+    image:
+      "/work/WEBSITE ART Beardy Bodo KT 052622 VOICE WRITING SERIES DEVELOPMENT.png",
+    featuresDesc: "Baboon adapted and voiced this Riki Group series.",
+    key: 11,
+    category: "voice, writing, series, development",
+  },
+  {
+    image: "/work/WEBSITE ART Beavis and Butthead CK 070822 SERIES.png",
+    featuresDesc: "Greg Grabiaski, Writer\nMike Seve, Director",
+    key: 12,
+    category: "writing, series",
+  },
+  {
+    image:
+      "/work/WEBSITE ART Beavis and Butthead Do America CK 070822 FEATURE.png",
+    featuresDesc: "Mike de Seve, Sequence Writer and Director",
+    key: 13,
+    category: "writing, features",
+  },
+  {
+    image:
+      "/work/WEBSITE ART Bek and the Bunnies KT 052622 V2 WRITING SERIES BRANDS DEVELOPMENT ORIGINALS.png",
+    featuresDesc:
+      "In this Baboon original with Toon2Tango, hilarious adventures ensue when Bek, a fun-loving 7-year-old, invents robot bunny friends to keep her company in her country home.",
+    key: 14,
+    category: "branded, deveoplment, originals",
+  },
+  {
+    image:
+      "/work/WEBSITE ART Best and Bester KT 060922 062222 SERIES DEVELOPMENT.png",
+    featuresDesc:
+      "Baboon script-doctored this Nickelodeon International series from Gigglebug Entertainment and Nelvana.",
+    key: 15,
+    category: "series, development",
+  },
+  {
+    image:
+      "/work/WEBSITE ART Bionic Max KT 052122 TK 060822 WRITING SERIES DEVELOPMENT.png",
+    featuresDesc:
+      "Mike de Seve, Joe Vitale, Jeff Hylton, wrote this Gaumont series for Cartoon Network Africa.",
+    key: 16,
+    category: "writing, series, development",
+  },
+  {
+    image: "/work/WEBSITE ART Bottle Cap KT 061422 FEATURE DEVELOPMENT.png",
+    featuresDesc:
+      "Baboon developed this short film, recipient of the Epic MegaGrant. ",
+    key: 17,
+    category: "features, development",
+  },
+  {
+    image:
+      "/work/WEBSITE ART Buck and Buddy KT 052622 WRITING SERIES BRANDS DEVELOPMENT.png",
+    featuresDesc:
+      "Baboon wrote and developed this 2D animatied series for Malaysian animation house Little Critter Workshop.",
+    key: 18,
+    category: "writing, series, branded, development",
+  },
+  {
+    image: "/work/WEBSITE ART Bug Bunny Builders KT 071222 WRITING SERIES.png",
+    featuresDesc: "John Reynolds, Writer",
+    key: 19,
+    category: "writing, series",
+  },
+  {
+    image: "",
+    featuresDesc: "",
+    key: 8,
+    category: "",
+  },
+  {
+    image: "",
+    featuresDesc: "",
+    key: 8,
+    category: "",
+  },
+  {
+    image: "",
+    featuresDesc: "",
+    key: 8,
+    category: "",
+  },
+  {
+    image: "",
+    featuresDesc: "",
+    key: 8,
+    category: "",
+  },
+  {
+    image: "",
+    featuresDesc: "",
+    key: 8,
+    category: "",
+  },
+  {
+    image: "",
+    featuresDesc: "",
+    key: 8,
+    category: "",
+  },
+  {
+    image: "",
+    featuresDesc: "",
+    key: 8,
+    category: "",
   },
 ];
